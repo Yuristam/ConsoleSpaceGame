0f6d41de5c1cd65595253be69b2096af79575802
Yuristam <yuristamasanakunov@gmail.com>
2023-02-17 18:49:50 +0600

    Add project files.

---
+++ ConsoleSpaceGame/Program.cs
@@ -0,0 +1,39 @@
+﻿// Player's ship positions (X => abcissa, Y => ordinate)
+int userX = 50, userY = 27;
+
+while (true)
+{
+    //========================================================================
+
+    // this is the borders of the map
+    for (int i = 0; i < 30; i++)
+    {
+        Console.SetCursorPosition(30, i);
+        Console.Write('|');
+        Console.SetCursorPosition(80, i);
+        Console.Write('|');
+    }
+    //========================================================================
+
+    // player's positions (that will be in use)
+    Console.SetCursorPosition(userX, userY);
+    Console.Write("-|[<^>]|-"); // player's ship
+
+    ConsoleKeyInfo player = Console.ReadKey();
+    switch (player.Key)
+    {
+        case ConsoleKey.LeftArrow:
+            if (userX - 1 != 30)
+            {
+                userX--; // move left
+            }
+            break;
+        case ConsoleKey.RightArrow:
+            if (userX + 1 != 72)
+            {
+                userX++; // move right 
+            }
+            break;
+    }
+    Console.Clear();
+}
